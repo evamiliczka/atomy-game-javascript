4ec75110e5c59af8c163e44613709b5e2cab2ef6
eva miliczka <eva.miliczka@gmail.com>
2022-07-30 15:23:35 +0200

    mm

---
+++ score.js
@@ -0,0 +1,69 @@
+/** Tu ukladame paramaetre a vsetko o hracoch.
+ * V inych suboroch hraca vidime ako cislo, momentlane 0 a 1
+ */
+
+ var Score = {
+    _players: [
+        {
+            color: "blue",
+            name: "Modry hrac",
+            score: 0,
+            node: null //kde v html ho budeme vypisovat
+        },
+        {
+            color: "red",
+            name: "Cerveny hrac",
+            score: 0,
+            node: null
+        }
+    ],
+    _gameOver: false //true, ak niektory hrac zaplni vsetky bunky
+}
+
+
+Score.init = function(){
+    for (var i=0; i<this._players.length; i++){
+        var tempPlayer = this._players[i];
+        tempPlayer.node = document.createElement("span");
+
+        var p = document.createElement("p");
+        p.style.color = tempPlayer.color;
+        p.appendChild(document.createTextNode(tempPlayer.name + ": "));
+        p.appendChild(tempPlayer.node);
+
+        document.body.appendChild(p);
+    }
+}
+
+Score.getColor = function(player){
+    return this._players[player].color;
+}
+
+Score.getPlayerCount = function(){
+    return this._players.length;
+}
+
+Score.isGameOVer = function(){
+    return this._gameOver;
+}
+
+Score.removePoint = function(player){
+    if (player == -1) {return;}
+    var tempPlayer = this._players[player];
+    tempPlayer.score--;
+    tempPlayer.node.innerHTML = tempPlayer.score;
+}
+
+Score.addPoint = function(player){
+    var tempPlayer = this._players[player];
+    tempPlayer.score++;
+    tempPlayer.node.innerHTML = tempPlayer.score;
+
+    /* Ak sme dosiahli max skore, t.j. hrac obsadil vsetky bunky, hra konci */
+    if (tempPlayer.score == Game.SIZE * Game.SIZE) {
+        Player.stopListening();
+        this._gameOver = true;
+        alert("Game over");
+    }
+}
+
